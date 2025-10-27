65d8729321401137ae6b2bcebc870c2b8f1ef0d2
Thorium3324 <118537377+Thorium3324@users.noreply.github.com>
2025-10-27 03:03:56 +0100

    utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,12 @@
+import streamlit as st
 import yfinance as yf
 import pandas as pd
 from ta.momentum import RSIIndicator
 from ta.trend import MACD, SMAIndicator, EMAIndicator
 from ta.volatility import BollingerBands
 
-def get_stock_data(ticker, start, end):
+@st.cache_data(ttl=3600)
+def get_stock_data_cached(ticker, start, end):
     df = yf.download(ticker, start=start, end=end)
     if not df.empty:
         df['MA20'] = SMAIndicator(df['Close'], 20).sma_indicator()
@@ -17,3 +19,8 @@ def get_stock_data(ticker, start, end):
         df['BB_upper'] = bb.bollinger_hband()
         df['BB_lower'] = bb.bollinger_lband()
     return df
+
+@st.cache_data(ttl=3600)
+def get_multiple_stock_data(tickers, start, end):
+    df_all = yf.download(tickers, start=start, end=end)['Close']
+    return df_all
